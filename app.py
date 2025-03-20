a4ec3444711ae861138c9f3ef4613a621746d249
HarshVelani <velaniharsh17@gmail.com>
2025-03-20 16:40:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from def_functions import *
+from prompts import *
+
+# Initialize ChatGroq LLM (Replace with your API key)
+# llama3-8b-8192
+# llama3-70b-8192
+# deepseek-r1-distill-llama-70b
+# qwen-2.5-32b
+
+llm = ChatGroq(groq_api_key="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", model_name="qwen-2.5-32b")
+embedding = load_embedding()
+load_vectorDB = load_data_from_VectorDB(embedding)
+memory = conversation_memory()
+
+while True:
+    
+    user_input = input("Enter: ")
+
+    if user_input.lower() in ["quit", "bye"]:
+        print("Goodbye!")
+        print("Chat Summary:", memory.load_memory_variables({})["history"])
+        break
+    
+    response = chat_with_llm(llm, SystemPrompt, user_input, memory)
+    print("1.======"+response)
+    print("\n=============================================\n")
+
+    if "MVA Sections = TRUE" in response:
+        
+        retrieval_chain = make_retieval_chain(llm, RetrievalPrompt, load_vectorDB)
+
+        chat_summary = memory.load_memory_variables({})["history"][0]        
+        history = chat_summary.content
+        response = retrieval_chain.invoke({"input":history})
+
+        # # Save conversation in memory
+        memory.save_context({"input": user_input}, {"output": response['answer']})
+
+        clean_retreived_response(response)
+        print("\n=============================================\n")
+        print("Goodbye!")
+        print("Chat Summary:", memory.load_memory_variables({})["history"])
+        break
+
+# challan was issued by traffic police even after i followed the traffic rules
+# please provide details about MVA Sections
---
+++ def_functions.py
@@ -0,0 +1,103 @@
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from qdrant_client import QdrantClient
+from langchain_community.vectorstores import Qdrant
+from langchain.schema import HumanMessage
+from langchain.memory import ConversationSummaryMemory
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import re
+
+
+
+def chat_with_llm(llm, prompt_template, user_input, memory):
+    # Retrieve past conversation history
+    history = memory.load_memory_variables({})["history"]
+    # Format the prompt with history and current question
+    prompt = prompt_template.format(history=history, question=user_input)
+    # Get response from LLM
+    response = llm.invoke([HumanMessage(content=prompt)])
+    # Save conversation in memory
+    memory.save_context({"input": user_input}, {"output": response.content})
+    return response.content
+
+
+
+def conversation_memory():
+    # Initialize ChatGroq LLM (Replace with your API key)
+    llm = ChatGroq(groq_api_key="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", model_name="llama3-8b-8192")
+
+    # Initialize memory to store conversation history
+    return ConversationSummaryMemory(llm=llm, return_messages=True)
+
+
+
+def quit_conversation(user_input, memory):
+    if user_input.lower() in ["quit", "bye"]:
+        print("Good Bye")
+        print("Chat Summary:")
+        print(memory.load_memory_variables({})["history"])
+        return False
+
+
+
+def load_embedding():
+    # LOCAL
+    model_name = 'BAAI/bge-base-en'
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+
+    embeddings= HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
+    print("\n============================= Embedding Model Loaded...=============================\n")
+    return embeddings
+
+
+
+def load_data_from_VectorDB(embeddings):
+    # Load Data from Vector-DB
+    url = "http://localhost:6333"
+    collection_name = "MVA_db_bge_base_en"
+
+    client = QdrantClient(
+        url = url,
+        prefer_grpc = False
+    )
+    # print(client)
+    print("\n============================= Client Loaded...=============================\n")
+
+    db = Qdrant(
+        client = client,
+        embeddings = embeddings,
+        collection_name = collection_name
+    )
+    print("\n============================= Data Loaded =============================\n")
+    return db
+
+
+
+def make_retieval_chain(llm, prompt, vector_data):
+    # Create document chain
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    # Input--->Retriever--->vectorstoredb
+    # Create Retrieve
+    retriever = vector_data.as_retriever(similarity_score_threshold=0.3)
+    retrieval_chain = create_retrieval_chain(retriever,document_chain)
+    return retrieval_chain
+
+
+
+def clean_retreived_response(response):
+    # Extract text from the response dictionary
+    raw_text = response['answer']
+    # Remove Markdown-style formatting (e.g., '**' for bold)
+    clean_text = re.sub(r'\*\*', '', raw_text)
+    print(clean_text)
+    print("\n============================= Response Loaded =============================\n")
