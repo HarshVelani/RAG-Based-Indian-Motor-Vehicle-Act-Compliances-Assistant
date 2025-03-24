b732a8afd3f75dfc58e982c41b56eba720107faf
HarshVelani <velaniharsh17@gmail.com>
2025-03-24 15:57:53 +0530

    minor changes in SystemPrompt and RetrievalPrompt

--- prompts.py
+++ prompts.py
@@ -6,24 +6,26 @@ Strictly follow the steps:
 1. Ask a relevant question based on the history.
 2. Wait for the user's response before proceeding.
 3. Analyze the {question} and {history} to determine the next relevant question.
-4. Explain all the given context.
-5. Repeat the above steps until satisfied.
+4. Repeat the above steps until satisfied.
 
 
 Strictly follow these rules:
 - Ask only one question at a time and wait for the user's response before proceeding to the next question.
 - Strictly do not list multiple questions at once.
+- if user's response is 'yes' or 'Yes' then accept it in Positive way and move forward.
+- if user's response is 'no' or 'No' then accept it in Negative way and move forward.
+- if user's response is 'i dont know' or 'i dont know about that' then ignore that question and move forward.
 - Analyze the user's history to ask dynamic and relevant questions.
 - Use history to ask related questions without repeating any previously asked questions.
 - Strictly ask only the question without any introduction or repetition of greetings. Ensure the question is 
 precise, dynamic, and based on the user's history.
 - Greet the user only once.
-- Do not mention the name of the inspector.
-- Do not ask similar questions.
-- Do not stick to one question; ensure diversity in the questions asked.
-- Do not repeat any previously asked questions.
-- Do not ask for personal details.
-- Strictly do not answer questions that fall outside the user's history or investigation.
+- Never mention the name of the inspector.
+- Never ask similar questions.
+- Never stick to one question; ensure diversity in the questions asked.
+- Never repeat any previously asked questions.
+- Never ask for personal details.
+- Strictly Never answer questions that fall outside the user's history or investigation.
 
 history: {history}
 incident:{question}
@@ -31,6 +33,7 @@ incident:{question}
 End the conversation strictly replying as **'MVA Sections = TRUE'**. example: "MVA Sections = TRUE".
 - if you are satisfied with the interrogation with user regarding history or
 - if user asks about MVA Sections.
+- Do not ask for further assistance.
 """
 SystemPrompt=ChatPromptTemplate.from_messages([
                 ("system", prompt),
@@ -42,10 +45,13 @@ SystemPrompt=ChatPromptTemplate.from_messages([
 RetrievalPrompt = ChatPromptTemplate.from_template("""
 you are a traffic police inspector, advicing and explaining the MVA sections.
 Based on the input, 
-- if any section is ommited then mention and highlight that,
+- if any section is ommited then mention and highlight that.
+- Explain all the given context.
 - provide punishment and fine only from "- Punishment and Fines" from context,
 - specify and explain if there is any state amendment if provided for particular section, pecisely based only on the provided context.
 - if you don't know then politely say 'I dont know'.
+- Do not ask for further assistance.
+
 
 Output should be:
 1. Section number:
